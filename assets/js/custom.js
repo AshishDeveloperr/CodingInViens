b4b478bdfa9f5f4ebe4ad752c2ef0cef1ccb78ae
AshishDeveloperr <84321657+AshishDeveloperr@users.noreply.github.com>
2022-01-10 11:08:55 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -86,3 +86,26 @@ new WOW().init();
 
 
 })(window.jQuery);
+
+  // prevent
+
+        $(window).on('keydown',function(event)
+        {
+        if(event.keyCode==123)
+        {
+            return false;
+        }
+        else if(event.ctrlKey && event.shiftKey && event.keyCode==73)
+        {
+            return false;  //Prevent from ctrl+shift+i
+        }
+        else if(event.ctrlKey && event.keyCode==73)
+        {
+            return false;  //Prevent from ctrl+shift+i
+        }
+        });
+        $(document).on("contextmenu",function(e)
+        {
+        e.preventDefault();
+        });
+                    
